9d0dad1acd12a2bde96c107018fb3a392f252262
itWelt <rassulovmurad@gmail.com>
2023-10-01 21:08:25 +0300

    first commit

---
+++ application/app/Models/Configuration.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\GenerateUuidModelTrait;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Configuration extends Model
+{
+    use HasFactory, GenerateUuidModelTrait;
+
+    protected $fillable = [
+        'uuid',
+        'amocrm_subdomain',
+        'amocrm_redirect_uri',
+        'amocrm_client_secret',
+        'amocrm_communication_methods_id',
+        'amocrm_social_links_id',
+        'amocrm_additional_info_id',
+        'amocrm_pregnancy_id',
+        'amocrm_human_parameters_id',
+        'amocrm_health_id',
+        'amocrm_family_status_id',
+        'amocrm_contact_person_id',
+        'amocrm_language_knowledge_id',
+        'amocrm_driver_license_id',
+        'amocrm_education_id',
+        'amocrm_visa_violations_id',
+        'amocrm_criminal_record_id',
+        'amocrm_previous_work_visas_id',
+        'amocrm_outside_recommendation_id',
+        'amocrm_experience_id',
+        'amocrm_last_3_jobs_id',
+        'amocrm_current_work_place_id',
+        'amocrm_residence_permit_id',
+        'amocrm_passport_id',
+        'amocrm_residence_id',
+        'amocrm_citizenship_id',
+        'amocrm_phone_id',
+        'amocrm_full_name_id',
+        'amocrm_position_id',
+        'amocrm_email_id',
+        'amocrm_default_resp_user_id',
+        'amocrm_form_sent_id',
+        'amocrm_form_completed_id',
+    ];
+    protected $hidden = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
+}
---
+++ application/routes/api.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Http\Controllers\API\V1\ConfigurationController__1;
+use App\Http\Controllers\API\V1\ServicesAmoCrmController__1;
+use App\Http\Controllers\API\V1\ServicesYandexFormsController__1;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('v1')->group(function () {
+    Route::prefix('services')->group(function () {
+        Route::post('/test', function (Request $request) {
+            return 'hallo';
+        });
+
+        Route::prefix('amocrm')->group(function () {
+            Route::prefix('auth')->group(function () {
+                Route::get('signin', [ServicesAmoCrmController__1::class, 'signin']);
+                Route::get('signout', [ServicesAmoCrmController__1::class, 'signout']);
+            });
+        });
+
+        Route::prefix('yandex')->group(function () {
+            Route::prefix('forms')->group(function () {
+                Route::middleware(['services.amo.token'])->post('submit', [ServicesYandexFormsController__1::class, 'submit']);
+            });
+        });
+    });
+
+    Route::prefix('admin')->group(function () { //FIXME es muss in der Zukunft verteidigt werden
+        Route::prefix('configurations')->group(function () {
+            Route::post('/', [ConfigurationController__1::class, 'create']);
+            Route::get('/', [ConfigurationController__1::class, 'read']);
+            Route::put('/', [ConfigurationController__1::class, 'update']);
+            Route::delete('/', [ConfigurationController__1::class, 'delete']);
+        });
+    });
+});
